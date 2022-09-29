fa6c30e5630e8d4c890550b1eebef77ac8afce58
kelsonbatista <kelsonbatista@gmail.com>
2022-09-29 18:03:59 -0300

    refactor: print removed in search by title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -4,7 +4,6 @@ from tech_news.database import search_news
 # Requisito 6
 def search_by_title(title):
     filter = search_news({"title": {"$regex": f"{title.lower()}"}})
-    print(filter)
     return [(item["title"], item["url"]) for item in filter]
 
 
