38a6869678f0f7aec8bfcce1f7e805411ac4bde8
kelsonbatista <kelsonbatista@gmail.com>
2022-10-02 02:18:16 -0300

    refactor: menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -6,76 +6,63 @@ from tech_news.analyzer.search_engine import (
     search_by_tag,
     search_by_category
 )
+import sys
 
 
 # Requisito 12
 def analyzer_menu():
     MENU_OPTIONS = """
-    Selecione uma das opções a seguir:
-    0 - Popular o banco com notícias;
-    1 - Buscar notícias por título;
-    2 - Buscar notícias por data;
-    3 - Buscar notícias por tag;
-    4 - Buscar notícias por categoria;
-    5 - Listar top 5 notícias;
-    6 - Listar top 5 categorias;
-    7 - Sair.
-    """
+Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por tag;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair."""
 
     option = input(MENU_OPTIONS)
+    selection = ["1", "2", "3", "4", "5", "6", "7"]
 
-    if int(option) == 0:
+    if option == "0":
         populate_news()
-    if 1 <= int(option) <= 4:
-        return menu_scrape(option)
-    if 5 <= int(option) <= 6:
-        return menu_search(option)
-    if option == 7:
-        return menu_exit()
+    elif option in selection:
+        try:
+            print(menu_scrape(option))
+        except Exception as err:
+            sys.stderr.write(err)
     else:
-        print("Opção inválida")
+        sys.stderr.write("Opção inválida\n")
 
 
 def populate_news():
     response = input("Digite quantas notícias serão buscadas:")
-    get_tech_news(int(response))
+    get_tech_news(response.isdigit())
     return
 
 
 def menu_scrape(option):
     if option == "1":
         response = input("Digite o título:")
-        print(search_by_title(response))
-        return
-
-    if option == "2":
+        return search_by_title(response)
+    elif option == "2":
         response = input("Digite a data no formato aaaa-mm-dd:")
-        print(search_by_date(response))
-        return
-
-    if option == "3":
+        return search_by_date(response)
+    elif option == "3":
         response = input("Digite a tag:")
-        print(search_by_tag(response))
-        return
-    if option == "4":
+        return search_by_tag(response)
+    elif option == "4":
         response = input("Digite a categoria:")
-        print(search_by_category(response))
-        return
+        return search_by_category(response)
+    else:
+        menu_search(option)
 
 
 def menu_search(option):
     if option == "5":
-        print(top_5_news())
-        return
-
-    if option == "6":
-        print(top_5_categories())
-        return
-
-
-def menu_exit():
-    print("Encerrando script")
-    return False
-
-
-analyzer_menu()
+        return top_5_news()
+    elif option == "6":
+        return top_5_categories()
+    elif option == "7":
+        print("Encerrando script\n")
